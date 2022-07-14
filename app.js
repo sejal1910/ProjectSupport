e619953cc69343ae44c426cef6b60576c241d4f1
sejal1910 <rajadhyakshasejal@gmail.com>
2022-07-14 12:56:35 +0530

    initialized and set up our server

---
+++ app.js
@@ -0,0 +1,20 @@
+//Call in installed dependencies
+const express=require('express');
+
+//set up express app
+const app=express()
+
+//set up port number
+const port=5035;
+
+// set up home route
+app.get('/',(req, res) => {
+  res.status(200).json({
+    message:'Welcome to Project Support',
+  });
+});
+
+//sever listening
+app.listen(port,(req,res)=>{
+    console.log('Our server is live on ${port}.Yay!');
+});
